92267544cbde20b6b8cd2d6dc812f9ab1809c77f
João Francisco <JF756061@gmail.com>
2026-05-02 05:47:35 -0300

    initial

---
+++ src/test/java/dev/joao/mastersys/MastersysApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.joao.mastersys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MastersysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
