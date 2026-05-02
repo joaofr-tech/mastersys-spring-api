30692a36838ce6b9a7faa18b460f4b820849454c
João Francisco <JF756061@gmail.com>
2026-05-02 06:23:48 -0300

    cria insert db v2

---
+++ src/main/resources/db/migration/V2__insert_dados_iniciais.sql
@@ -0,0 +1,24 @@
+INSERT INTO modalidades (nome) VALUES 
+('Musculacao'),
+('Funcional'),
+('Jiu-Jitsu'),
+('Muay Thai'),
+('Pilates');
+
+INSERT INTO planos (modalidade_id, nome, valor_mensal)  
+SELECT id 'Mensal', 120.00 FROM modalidades WHERE nome = 'Musculacao';
+
+INSERT INTO planos (modalidade_id, nome, valor_mensal)  
+SELECT id 'Trimestral', 330.00 FROM modalidades WHERE nome = 'Musculacao';
+
+INSERT INTO planos (modalidade_id, nome, valor_mensal)  
+SELECT id 'Mensal', 150.00 FROM modalidades WHERE nome = 'Funcional';
+
+INSERT INTO planos (modalidade_id, nome, valor_mensal)  
+SELECT id 'Mensal', 180.00 FROM modalidades WHERE nome = 'Jiu-Jitsu';
+
+INSERT INTO graduacoes (modalidade_id, nome)  
+SELECT id 'Faixa Branca'FROM modalidades WHERE nome = 'Jiu-Jitsu';
+
+INSERT INTO graduacoes (modalidade_id, nome)  
+SELECT id 'Faixa Branca'FROM modalidades WHERE nome = 'Jiu-Jitsu';
